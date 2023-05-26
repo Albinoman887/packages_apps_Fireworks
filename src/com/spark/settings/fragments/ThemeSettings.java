91dc185e1b04d7932eb1d7ab3c71b74aff201c8a
ariffjenong <arifbuditantodablekk@gmail.com>
2023-05-26 12:27:08 -0700

    settings: Add QS Page Custom Transition Animations [2/2]
    Signed-off-by: ariffjenong <arifbuditantodablekk@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/spark/settings/fragments/ThemeSettings.java
+++ src/com/spark/settings/fragments/ThemeSettings.java
@@ -70,6 +70,7 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String QS_PANEL_STYLE = "qs_panel_style";
+    private static final String QS_PAGE_TRANSITIONS = "custom_transitions_page_tile";
 
     // Heads-up notification constants
     private static final String HEADS_UP_TIMEOUT_PREF = "heads_up_timeout";
@@ -116,6 +117,8 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
     private SwitchPreference mShowAutoBrightness;
+    private SystemSettingListPreference mPageTransitions;
+
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -204,6 +207,14 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
 
         mQsStyle = (SystemSettingListPreference) findPreference(QS_PANEL_STYLE);
         mCustomSettingsObserver.observe();
+
+        mPageTransitions = (SystemSettingListPreference) findPreference(QS_PAGE_TRANSITIONS);
+        mPageTransitions.setOnPreferenceChangeListener(this);
+        int customTransitions = Settings.System.getIntForUser(resolver,
+                Settings.System.CUSTOM_TRANSITIONS_KEY,
+                0, UserHandle.USER_CURRENT);
+        mPageTransitions.setValue(String.valueOf(customTransitions));
+        mPageTransitions.setSummary(mPageTransitions.getEntry());
     }
 
     private static boolean isAudioPanelOnLeftSide(Context context) {
@@ -278,6 +289,14 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
             return true;
         } else if (preference == mQsStyle) {
             mCustomSettingsObserver.observe();
+            return true;
+        } else if (preference == mPageTransitions) {
+            int customTransitions = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.CUSTOM_TRANSITIONS_KEY, customTransitions, UserHandle.USER_CURRENT);
+            int index = mPageTransitions.findIndexOfValue((String) newValue);
+            mPageTransitions.setSummary(
+                    mPageTransitions.getEntries()[index]);
             return true;
         }
          return false;
